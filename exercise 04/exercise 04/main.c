9f3d82771baf8a89d7193ba6a864a6d41d343934
seono9y <144989211+seono9y@users.noreply.github.com>
2023-10-08 16:40:49 +0900

    01

---
+++ exercise 04/exercise 04/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  exercise 04
+//
+//  Created by 추서연 on 2023/10/08.
+//
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{ int x, y = 0, z, m = 0;
+    int a, b, c;
+    x = 2;
+    z = 1;
+    a = 3;
+    b = 4;
+    c = 5;
+    
+    // y = ax^2 + bx + c
+    // m = x+y+z/3
+    
+    printf("y=%d, m=%d", y, m);
+}
